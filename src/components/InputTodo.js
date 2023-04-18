cf3ceae78c41bd2d5fdae86612e70717f7d6fe1a
arly <astra.xvi@gmail.com>
2023-04-18 11:03:31 +0800

    initial commit

---
+++ src/components/InputTodo.js
@@ -0,0 +1,36 @@
+import React, { Fragment, useState } from "react";
+import axios from 'axios';
+
+const InputTodo = () => {
+
+    const [description, setDescription] = useState("");
+
+    const onSubmitForm = async e => {
+        e.preventDefault();
+        try {
+            await axios.post('http://localhost:5000/todos', {
+                description
+            });
+            setDescription('');
+            window.location = "/";
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
+    return (
+        <Fragment>
+            <h1 className="text-center mt-5"> PERN Todo List </h1>
+            <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+                <input
+                    type="text"
+                    className="form-control"
+                    value={description}
+                    onChange={e => setDescription(e.target.value)} />
+                <button className="btn btn-success">Add</button>
+            </form>
+        </Fragment>
+
+    );
+
+};
+export default InputTodo;
